55c1550d96fa2029205db0e207affd322f3a8b79
YuriiFreitass <yurifreitas1501@gmail.com>
2026-06-20 13:18:48 -0300

    Adiciona exercicio 03 - controle de fluxo

---
+++ src/logica/exercicio03/Main.java
@@ -0,0 +1,17 @@
+package logica.exercicio03;
+
+public class Main {
+	public static void main(String[] args) {
+		String nome = "Maria";
+		int idade = 10;
+
+		if (idade >= 18) {
+			System.out.println("Nome: " + nome + " \nIdade: " + idade + " \nStatus: A entrada foi permitida") ;
+
+		} else {
+			System.out.println("Nome: " + nome + " \nIdade: " + idade + " \nStatus: A entrada foi negada.") ;
+
+		}
+
+	}
+}
